562eb21aa83043648a54a6a04fc66d6715df5df3
NaseerHines <Hinesnaseer@gmail.com>
2020-04-18 12:56:09 -0500

    Working on getting server and db connected and running

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const parser = require("body-parser");
+const path = require("path");
+const { apiRouter } = require("./api/index");
+
+const app = express();
+
+const PORT = 8080;
+const CLIENT_PATH = path.join(__dirname, "../client/dist");
+
+app.use(express.static(CLIENT_PATH));
+app.use(parser.json());
+app.use("/api", apiRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on :${PORT} 🚀`);
+});
+
+module.exports.app = app;
