13a61cd4ca19971eeda09772f747d115f966f1b4
NaseerHines <Hinesnaseer@gmail.com>
2020-05-01 18:22:40 -0500

    Setting up server to api requests

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,35 @@
 const { Router } = require("express");
-
+const axios = require("axios");
+const { token } = require('../config');
+const { pullAccount, saveAccount } = require('../db/index')
 const apiRouter = Router();
+
+const grabAccount = (username, platform, cb) => {
+  const apiUrl = `https://api2.r6stats.com/public-api/stats/${username}/${platform}/generic`;
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return axios
+    .get(apiUrl, options)
+    .then((account) => {
+      const stats = account.data.stats.general;
+      cb(stats);
+    })
+    .catch((err) => {
+      console.log(err, "didn't work");
+    });
+};
+
+apiRouter.post("/account", (req, res) => {
+  const { username, platform } = req.body;
+  console.log(username, platform);
+  grabAccount(username, platform, (data) => {
+    console.log(data);
+    saveAccount(data);
+    res.send(data);
+  });
+});
 
 module.exports.apiRouter = apiRouter;
