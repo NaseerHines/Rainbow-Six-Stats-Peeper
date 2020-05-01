62c4c04af20db822207a0ffeb88482b271e5cc6a
NaseerHines <Hinesnaseer@gmail.com>
2020-05-01 18:23:16 -0500

    Added functions to save and pull accounts form database

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,14 @@
 const mysql = require("mysql");
-
 const DB_HOST = "localhost";
 const DB_USER = "root";
 const DB_PASS = "";
-const DB_NAME = "mvp";
-
+const DB_NAME = "siege";
 const connection = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
 });
-
 connection.connect((err) => {
   if (err) {
     console.log(err);
@@ -19,3 +16,27 @@ connection.connect((err) => {
     console.log("Database connected");
   }
 });
+const pullAccount = (callback) => {
+  connection.query("SELECT * FROM Account", (err, result) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log(result);
+    callback(result);
+  });
+};
+const saveAccount = (account) => {
+  console.log(account);
+  if (account) {
+    const { username, platform, kd, wl, playtime, searchCount } = account;
+    const sql = `INSERT INTO Account(username, platform, kdRatio, wlRatio, playtime, searchedCount) VALUES('${username}', '${platform}', ${kd}, ${wl}, '${playtime}', '${searchCount}')`;
+    connection.query(sql, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    console.log("saved account");
+  }
+};
+module.exports.pullAccount = pullAccount;
+module.exports.saveAccount = saveAccount;
