74114960d1cc5129f2e1d0c51ed02305003ace95
NaseerHines <Hinesnaseer@gmail.com>
2020-05-01 17:59:19 -0500

    Actually can see harcoded data of stats

---
+++ src/components/Stats.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Lookup An Account's Stats</h2>
+    <div v-bind:key="stat.id" v-for="stat in stats">
+      <h3>Viewing {{stat.account}} Stats</h3>
+        <h4>Platform: {{stat.platform}}</h4>
+        <h4>Hours: {{stat.hours}}</h4>
+        <h4>Kills to Deaths: {{stat.kd}}</h4>
+        <h4>Wins To Loses: {{stat.wl}}</h4>
+    </div>
+  </div>
+</template>
+
+<script> 
+  export default {
+    name: 'Stats',
+    props: ["stats"]
+  }
+</script>
+
+<style scoped> 
+
+</style>
