1a3548b838ed142523337821a52bd568edfc242e
NaseerHines <Hinesnaseer@gmail.com>
2020-05-15 12:46:22 -0500

    new schema

--- schema.sql
+++ schema.sql
@@ -8,13 +8,45 @@ CREATE DATABASE siege;
 
 USE siege;
 
-CREATE TABLE `account` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`username` VARCHAR(250) NOT NULL,
-	`platform` VARCHAR(15),
-	`hours` INT,
-	`kdratio` INT,
-	`wlratio` INT,
-
-	PRIMARY KEY(`id`)
+CREATE TABLE `Account` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(55),
+  `platform` varcharacter(15)
 );
+
+CREATE TABLE `Team` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `squadLeader` int,
+  `playerOne` varchar(255),
+  `playerTwo` varchar(255),
+  `playerThree` varchar(255),
+  `playerFour` varchar(255)
+);
+
+CREATE TABLE `Favorites` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `account` int,
+  `favoritesList` varchar(255)
+);
+
+CREATE TABLE `History` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `account` int,
+  `historyList` varchar(255)
+);
+
+CREATE TABLE `Stats` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `account` int,
+  `kdratio` int,
+  `wlratio` int,
+  `playtime` int
+);
+
+ALTER TABLE `Account` ADD FOREIGN KEY (`id`) REFERENCES `History` (`account`);
+
+ALTER TABLE `Account` ADD FOREIGN KEY (`id`) REFERENCES `Team` (`squadLeader`);
+
+ALTER TABLE `Account` ADD FOREIGN KEY (`id`) REFERENCES `Stats` (`account`);
+
+ALTER TABLE `Account` ADD FOREIGN KEY (`id`) REFERENCES `Favorites` (`account`);
